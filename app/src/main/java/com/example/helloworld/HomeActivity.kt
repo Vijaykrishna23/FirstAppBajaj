8caf6842544ccdd1e62a9660fce951cfbebaebe5
Vijaykrishna23 <vk1500@srmist.edu.in>
2022-01-28 08:47:57 +0530

    start activity for result

--- app/src/main/java/com/example/helloworld/HomeActivity.kt
+++ app/src/main/java/com/example/helloworld/HomeActivity.kt
@@ -1,20 +1,51 @@
 package com.example.helloworld
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var tvHome: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var tvHome = findViewById<TextView>(R.id.tvHome)
+        var name = intent.extras?.getString("vk")
+        tvHome.text = name
+
+
     }
 
     fun handleClick(view: android.view.View) {
-        Toast.makeText(this, "pressing...", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        when(view.id){
+            R.id.btnSend -> {  startDialer() }
+            R.id.btnMain -> {startMain() }
+        }
+    }
 
+    private fun startDialer() {
+        var dialIntent= Intent(Intent.ACTION_DIAL, Uri.parse("tel:21345678"))
+        startActivity(dialIntent)
+    }
+
+    private fun startMain() {
+        var intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("kv", "krishna vijay")
+        startActivityForResult(intent,123)
+    }
+
+
+    //result will arrive here -- photo, contact
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 123){ //RESULT_OK means its good to consume, 123 -- data being returned is of type contact
+            tvHome.text = data?.extras?.getString("con")
+        }
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +16,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textview = findViewById(R.id.tvMain);
+        String name = getIntent().getExtras().getString("kv");
+        textview.setText(name);
     }
 
     public void handleClick(View view) {
-        Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(this,HomeActivity.class));
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                startHome();
+                break;
+            case R.id.btnCancel:
+                createAlarm("bajaj", 14, 29);
+                break;
+            case R.id.btnFinish:
+                sendContact();
+                break;
+        }
     }
+
+    private void sendContact() {
+        //get the contact from edittext
+        EditText nameEditText = findViewById(R.id.etName);
+        String contact = nameEditText.getText().toString();
+        //put the contact in intent
+        Intent cIntent = new Intent();
+        cIntent.putExtra("con", contact);
+        //set the result
+        setResult(RESULT_OK, cIntent);
+        //close this activity
+        finish();
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void startHome() {
+        Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
+        Intent mIntent;
+        mIntent = new Intent(MainActivity.this, HomeActivity.class);
+
+        mIntent.putExtra("vk", "vijay krishna");
+        startActivity(mIntent);
+    }
+
 
 }
